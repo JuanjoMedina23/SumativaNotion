5d28d0e861460ae00fa8516bfaad0ed796804115
jjmmhola2 <jjmmhola2@gmail.com>
2025-11-22 18:56:21 +0000

    🔧 chore: configurar index para redirigir según el estado de autenticación  -Se agrega lógica para enviar al usuario a /auth/login o /notes   dependiendo de si está autenticado o no  - Uso de useAuth y Redirect para manejar la navegación inicial

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+// app/index.tsx
+import { Redirect } from "expo-router";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const { user } = useAuth();
+
+  // Si hay usuario → ir a notas
+  // Si no → login
+  return user ? <Redirect href="/notes" /> : <Redirect href="/auth/login" />;
 }
+
